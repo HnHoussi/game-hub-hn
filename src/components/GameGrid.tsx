eb67324be55a0079b8465c03af2fd3a139c08aca
HnHoussi <houcinehannoun@gmail.com>
2025-11-03 13:55:08 +0100

    create custom hook for fetching games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,31 +1,14 @@
-import {useEffect, useState} from 'react';
-import apiClient from "@/services/api-client.ts";
 import { Text } from "@chakra-ui/react"
+import useGames from "@/hooks/useGames.ts";
+
 
 const GameGrid = () => {
 
-    interface Game {
-        id: number;
-        name: string;
-    }
-
-    interface FetchGamesResponse {
-        count: number;
-        results: Game[];
-    }
-
-    const [games, setGames] = useState<Game[]>([]);
-    const [error, setError] = useState('');
-
-    useEffect(() => {
-        apiClient.get<FetchGamesResponse>('/games')
-            .then(res => setGames(res.data.results))
-            .catch(error => setError(error.message));
-    })
+    const {games, error} = useGames();
 
     return (
         <>
-        {error && <Text>{error}</Text>}
+            {error && <Text>{error}</Text>}
             <ul>
                 {games.map(game => <li key={game.id}>{game.name}</li>)}
             </ul>
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,50 @@
+import {useEffect, useState} from "react";
+import apiClient from "@/services/api-client.ts";
+import {CanceledError} from "axios";
+
+// Define the shape of a single game object
+interface Game {
+    id: number;
+    name: string;
+}
+
+// Define what the API returns when fetching games
+interface FetchGamesResponse {
+    count: number;
+    results: Game[];
+}
+
+// Custom Hook for fetching a list of games from the API
+const useGames = () => {
+
+    // stores fetched game data
+    const [games, setGames] = useState<Game[]>([]);
+    // stores any potential error message from the request
+    const [error, setError] = useState('');
+
+    // useEffect runs once when the component using this hook mounts
+    useEffect(() => {
+
+        // Create an AbortController to cancel the request if the component unmounts
+        const controller = new AbortController();
+
+        // Make GET request to the /games endpoint
+        apiClient.get<FetchGamesResponse>('/games', {signal: controller.signal})
+            // Store the list of games from the API response
+            .then(res => setGames(res.data.results))
+            .catch((error) => {
+                // If the request was canceled, do nothing
+                if (error instanceof CanceledError) return;
+                // Otherwise, store the error message
+                setError(error.message)
+            });
+
+        // Cleanup: abort the API request if the component unmounts early
+        return () => controller.abort();
+    }, []); // [] ensures this effect runs only once (on mount)
+
+    // Return the data and error so components can use them
+    return { games, error };
+}
+
+export default useGames;
