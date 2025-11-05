9a31d6d5fd7d446db59bc91a7bcfca1bfaa86f45
HnHoussi <houcinehannoun@gmail.com>
2025-11-05 11:19:22 +0100

    fix bugs

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <Provider>
+      <Provider >
         <App />
       </Provider>
   </StrictMode>,
